3c314f97b52e844eef76f55a61513e4277ba24f5
Larry <709603507@qq.com>
2018-12-12 11:45:39 +0800

    add pwm

---
+++ system_ability_2018/ip_repo/PWM/PWM.srcs/sources_1/new/PWM.v
@@ -0,0 +1,28 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2018/12/12 09:24:46
+// Design Name: 
+// Module Name: PWM
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module PWM(
+    input clk,
+    
+    
+    );
+endmodule
